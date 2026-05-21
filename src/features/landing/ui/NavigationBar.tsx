42d7af7b5d7171f004c173526362d6c901b3f50b
5hurik41 <lovegoodandyh@gmail.com>
2026-05-21 15:09:24 +0300

    ui: animate navigation bar entrance :art:

--- src/features/landing/ui/NavigationBar.tsx
+++ src/features/landing/ui/NavigationBar.tsx
@@ -1,4 +1,5 @@
 import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import { motion } from "framer-motion";
 
 function NavigationBar() {
   const menuItems = [
@@ -8,22 +9,31 @@ function NavigationBar() {
     { label: "CONTACTS", href: "#contacts" },
   ];
   return (
-    <NavigationMenu.Root className="w-full py-4 border-b-2 border-brand-dark font-semibold sticky">
-      <NavigationMenu.List className="w-full flex justify-between">
-        <div className="w-full flex gap-4 md:gap-14">
-          {menuItems.slice(0, 3).map((item, index) => (
-            <NavigationMenu.Item key={index}>
-              <NavigationMenu.Link href={item.href}>{item.label}</NavigationMenu.Link>
-            </NavigationMenu.Item>
-          ))}
-        </div>
-        <NavigationMenu.Item>
-          <NavigationMenu.Link href={menuItems.at(menuItems.length - 1)?.href}>
-            {menuItems.at(menuItems.length - 1)?.label}
-          </NavigationMenu.Link>
-        </NavigationMenu.Item>
-      </NavigationMenu.List>
-    </NavigationMenu.Root>
+    <motion.header
+      initial={{ opacity: 0, y: -20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{
+        duration: 0.8,
+        ease: [0.16, 1, 0.3, 1],
+      }}
+    >
+      <NavigationMenu.Root className="w-full py-4 border-b-2 border-brand-dark font-semibold sticky text-xl">
+        <NavigationMenu.List className="w-full flex justify-between">
+          <div className="w-full flex gap-8 md:gap-14">
+            {menuItems.slice(0, 3).map((item, index) => (
+              <NavigationMenu.Item key={index}>
+                <NavigationMenu.Link href={item.href}>{item.label}</NavigationMenu.Link>
+              </NavigationMenu.Item>
+            ))}
+          </div>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link href={menuItems.at(menuItems.length - 1)?.href}>
+              {menuItems.at(menuItems.length - 1)?.label}
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
+    </motion.header>
   );
 }
 
