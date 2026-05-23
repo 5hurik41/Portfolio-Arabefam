fceffc880c46339250bd0268145595b6c42ccf1c
5hurik41 <lovegoodandyh@gmail.com>
2026-05-23 16:39:32 +0300

    ui: add button discover :art:

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import Button from '../ui/Button.vue'
 import Flower from '../ui/Flower.vue'
 import Grid from '../ui/Grid.vue'
 import HeroTitle from '../ui/HeroTitle.vue'
@@ -9,4 +10,7 @@ import HeroTitle from '../ui/HeroTitle.vue'
   <Flower class="w-52 md:w-64 lg:w-72 absolute bottom-5 left-0 -translate-x-1/2 opacity-80" />
   <Flower class="w-60 md:w-80 lg:w-96 absolute top-10 right-0 translate-x-1/2 opacity-80" />
   <HeroTitle />
+  <footer class="absolute bottom-14 left-1/2 -translate-x-1/2">
+    <Button />
+  </footer>
 </template>
---
+++ src/components/ui/Button.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button
+    class="flex gap-2 justify-center items-center text-lg font-semibold px-4 py-2 bg-white/10 backdrop-blur-md border border-white/50 rounded-full"
+  >
+    <span> Discover </span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="w-5 h-5 animate-bounce mt-1"
+    >
+      <path d="m6 9 6 6 6-6" />
+    </svg>
+  </button>
+</template>
