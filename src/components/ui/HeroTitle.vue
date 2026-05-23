a1f4dd914286249aaddee7d922adcb67bbae0475
5hurik41 <lovegoodandyh@gmail.com>
2026-05-23 15:52:22 +0300

    ui: add hero section title CREATIVE DEVELOPERR :art:

--- src/components/ui/HeroTitle.vue
+++ src/components/ui/HeroTitle.vue
@@ -5,21 +5,24 @@ import star from '../../assets/star.svg'
 
 <template>
   <div
-    class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 font-extrabold select-none leading-none text-8xl md:text-[12vw] whitespace-nowrap"
+    class="absolute flex flex-col items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 select-none leading-none whitespace-nowrap"
   >
-    <span class="relative inline-block">
-      P
-      <img
-        :src="flower"
-        class="absolute top-[15%] left-[10%] w-[0.5em] -translate-x-1/2 -translate-y-1/2"
-      />
-    </span>
+    <p class="text-3xl md:text-[3vw] font-medium">Creative Developer</p>
+    <div class="text-8xl md:text-[12vw] font-extrabold">
+      <span class="relative inline-block">
+        P
+        <img
+          :src="flower"
+          class="absolute top-[15%] left-[10%] w-[0.5em] -translate-x-1/2 -translate-y-1/2"
+        />
+      </span>
 
-    <span>ortfol</span>
+      <span>ortfol</span>
 
-    <span class="relative inline-block">
-      ı
-      <img :src="star" class="absolute -top-2 left-1/2 -translate-x-1/2 w-[0.4em]" /> </span
-    >o
+      <span class="relative inline-block">
+        ı
+        <img :src="star" class="absolute -top-2 left-1/2 -translate-x-1/2 w-[0.4em]" /> </span
+      >o
+    </div>
   </div>
 </template>
