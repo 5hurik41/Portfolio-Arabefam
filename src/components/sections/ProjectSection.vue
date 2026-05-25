2384f4c58f9d0c22cefb810bd8082449bdf77f0e
5hurik41 <lovegoodandyh@gmail.com>
2026-05-25 09:12:32 +0300

    feat: init project section :wrench:

---
+++ src/components/sections/ProjectSection.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Grid from '../ui/Grid.vue'
+</script>
+
+<template>
+  <section
+    class="relative z-20 flex h-screen w-screen snap-start items-center justify-center overflow-hidden"
+  >
+    <Grid />
+    Projects
+  </section>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import AboutSeection from '@/components/sections/AboutSection.vue'
 import HeroSection from '@/components/sections/HeroSection.vue'
+import ProjectSection from '@/components/sections/ProjectSection.vue'
 </script>
 <template>
   <main class="relative h-screen w-full snap-y snap-mandatory overflow-x-hidden overflow-y-scroll">
     <HeroSection />
 
     <AboutSeection />
+
+    <ProjectSection />
   </main>
 </template>
